f0e44f8cc8bcde79361868ebd85322c086fe4050
johndpjr <johndpowell02@gmail.com>
2021-11-21 18:47:35 -0600

    centralize Entry object creation into _add_new_entry_list_item

--- controller.py
+++ controller.py
@@ -19,50 +19,51 @@ class Controller:
     def __init__(self, parent: App):
         self.parent = parent
 
+        self.db = Database()
+
         # "Global" variables
         self.title_entry_var = tk.StringVar()
 
-        self.db = Database()
         self.curr_entry = None
-        self.entries = {}  # contains active Entry objects
-        self._row = 2  # "New entry" button is above
+        self.entries = {}  # contains cached Entry objects
+        self._curr_grid_row = 2
     
     def add_frames(self):
         self.entry_list_frame = self.parent.entry_list_frame
         self.entry_frame = self.parent.entry_frame
     
     def delete_entry(self, entry: Entry):
-        # Remove widgets from view
+        """Removes the entry from the view and the database."""
         entry.remove_from_view()
-        # Remove from database
-        self.db.delete_entry(entry.uid)
-        del self.entries[entry.uid]
+
+        self.db.delete_entry(entry)
+        del self.entries[entry.uid]  # free entry from cache
     
-    def _new_entry_list_item(self):
+    def _add_new_entry_list_item(self, **kwargs):
         """Adds a new entry list item to the entry list.
-        Returns the created button and delete button.
+        Returns the newly created entry.
         """
         bttn = ttk.Button(self.entry_list_frame)
-        bttn.grid(row=self._row, column=0, sticky=tk.EW)
+        bttn.grid(row=self._curr_grid_row, column=0, sticky=tk.EW)
 
         del_bttn = ttk.Button(self.entry_list_frame, text='X')
-        del_bttn.grid(row=self._row, column=1)
-        self._row += 1
+        del_bttn.grid(row=self._curr_grid_row, column=1)
+        self._curr_grid_row += 1
 
-        return bttn, del_bttn
+        entry = Entry(self, bttn=bttn, del_bttn=del_bttn, **kwargs)
+        
+        return entry
     
     def on_startup(self):
         """Executes the startup flow for the application."""
         for entry in self.db.getall_entries():
             # Create entry object
-            bttn, del_bttn = self._new_entry_list_item()
-            e = Entry(self, uid=entry[0], title=entry[1],
-                      created_date=entry[2], content=entry[3],
-                      bttn=bttn, del_bttn=del_bttn,
-                      persistent=True)
+            entry = self._add_new_entry_list_item(uid=entry[0], title=entry[1],
+                                                  created_date=entry[2], content=entry[3],
+                                                  persistent=True
+            )
             # Update the dictionary
-            self.entries.update({e.uid: e})
-            bttn['text'] = e.title
+            self.entries.update({entry.uid: entry})
     
     def open_entry(self, entry):
         # Save the current entry
@@ -96,13 +97,10 @@ class Controller:
        
         # Focus on entry title
         self.entry_frame.title_entry.focus()
-       
-        bttn, del_bttn = self._new_entry_list_item()
 
         # Create Entry object and add to entries dict
-        self.curr_entry = Entry(self, uid=self.db.get_uid(),
-                                created_date=created_date,
-                                bttn=bttn, del_bttn=del_bttn
+        self.curr_entry = self._add_new_entry_list_item(uid=self.db.get_uid(),
+                                                        created_date=created_date
         )
         self.entries.update({self.curr_entry.uid: self.curr_entry})
     
